aa159369a1f2c995e3adec1da7a2f730894a747b
Guest User <Guest@epicodus-8.local>
2018-01-09 16:55:03 -0800

    all tests are passing

--- ToDoList.Tests/ControllerTests/ItemsControllerTest.cs
+++ ToDoList.Tests/ControllerTests/ItemsControllerTest.cs
@@ -1,34 +1,116 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ToDoList.Controllers;
 using ToDoList.Models;
+using ToDoList.Controllers;
+using Moq;
+using System.Linq;
 
-
-
-namespace ToDoList.Tests
+namespace ToDoList.Tests.ControllerTests
 {
+
     [TestClass]
-    public class ItemsControllerTest
+    public class ItemsControllerTests
     {
+        Mock<IItemRepository> mock = new Mock<IItemRepository>();
+
+        private void DbSetup()
+        {
+            mock.Setup(m => m.Items).Returns(new Item[]
+            {
+                new Item {ItemId = 1, Description = "Wash the dog" },
+                new Item {ItemId = 2, Description = "Do the dishes" },
+                new Item {ItemId = 3, Description = "Sweep the floor" }
+            }.AsQueryable());
+        }
+
+
         [TestMethod]
-        public void ItemsController_AddsItemToIndexModelData_Collection()
+        public void Mock_GetViewResultIndex_ActionResult() // Confirms route returns view
+        {
+            //Arrange
+            DbSetup();
+            ItemsController controller = new ItemsController(mock.Object);
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(ActionResult));
+        }
+
+        [TestMethod]
+        public void Mock_IndexContainsModelData_List() // Confirms model as list of items
         {
             // Arrange
-            ItemsController controller = new ItemsController();
-            Item testItem = new Item();
-            testItem.Description = "test item";
-            testItem.CategoryId = 1;
+            DbSetup();
+            ViewResult indexView = new ItemsController(mock.Object).Index() as ViewResult;
 
             // Act
-            controller.Create(testItem);
-            ViewResult indexView = new ItemsController().Index() as ViewResult;
-            var collection = indexView.ViewData.Model as List<Item>;
+            var result = indexView.ViewData.Model;
 
-           
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(List<Item>));
+        }
+
+        [TestMethod]
+        public void Mock_IndexModelContainsItems_Collection() // Confirms presence of known entry
+        {
+            // Arrange
+            DbSetup();
+            ItemsController controller = new ItemsController(mock.Object);
+            Item testItem = new Item();
+            testItem.Description = "Wash the dog";
+            testItem.ItemId = 1;
+
+            // Act
+            ViewResult indexView = controller.Index() as ViewResult;
+            List<Item> collection = indexView.ViewData.Model as List<Item>;
+
             // Assert
             CollectionAssert.Contains(collection, testItem);
+        }
+        [TestMethod]
+        public void Mock_PostViewResultCreate_ViewResult()
+        {
+            // Arrange
+            Item testItem = new Item
+            {
+                ItemId = 1,
+                Description = "Wash the dog"
+            };
+
+            DbSetup();
+            ItemsController controller = new ItemsController(mock.Object);
+
+            // Act
+            var resultView = controller.Create(testItem) as ViewResult;
+
+
+            // Assert
+            Assert.IsInstanceOfType(resultView, typeof(ViewResult));
+
+        }
+        [TestMethod]
+        public void Mock_GetDetails_ReturnsView()
+        {
+            // Arrange
+            Item testItem = new Item
+            {
+                ItemId = 1,
+                Description = "Wash the dog"
+            };
+
+            DbSetup();
+            ItemsController controller = new ItemsController(mock.Object);
+
+            // Act
+            var resultView = controller.Details(testItem.ItemId) as ViewResult;
+            var model = resultView.ViewData.Model as Item;
+
+            // Assert
+            Assert.IsInstanceOfType(resultView, typeof(ViewResult));
+            Assert.IsInstanceOfType(model, typeof(Item));
         }
     }
 }
